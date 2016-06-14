e029bd5ec6a10dce1ef2f5afd6f15efefe74f019
AlessandroBregoli <alessandroxciv@gmail.com>
2016-06-14 19:36:48 +0200

    Mancava un pezzo al commit di prima

---
+++ src/it/unimib/disco/ab/entityTopicStatistics/NerStats.java
@@ -0,0 +1,11 @@
+package it.unimib.disco.ab.entityTopicStatistics;
+
+import it.unimib.disco.ab.ner.CustomEntity;
+
+import java.io.Serializable;
+import java.util.TreeMap;
+
+public class NerStats implements Serializable{
+	public TreeMap<CustomEntity, TopicStat> relation = null;;
+	public long sentencePerTopic[] = null;
+}
